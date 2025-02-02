21f8467e20882ad14947676be0cf5516fb46912e
Suyeon-B <tndus4243@naver.com>
2025-02-02 18:06:41 +0900

    🔧 Chore: MSW를 이용한 모의 서버 환경 설정

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,19 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
-);
+async function initializeMockServiceWorker() {
+  const { setupWorker } = await import('msw/browser');
+  const { mockApiHandlers } = await import('./__mocks__/handlers.ts');
+  const worker = setupWorker(...mockApiHandlers);
+  return worker.start();
+}
+
+initializeMockServiceWorker().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </React.StrictMode>
+  );
+});
